8ab95b1418c1426222ed5af0d1b689432b5ea829
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-10 18:13:49 +0545

    feat: add FunRestController with /hello endpoint returning "Hello World!"

---
+++ src/main/java/np/com/krishnabk/springboothibernate/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,15 @@
+package np.com.krishnabk.springboothibernate.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // Expose "/hello" endpoint that returns "Hello World!"
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello World!";
+    }
+}

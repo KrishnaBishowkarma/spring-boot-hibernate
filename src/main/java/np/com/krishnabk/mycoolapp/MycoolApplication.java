2cb023b7e14556ab280a655a04b2d88fd762f8d0
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-11 15:42:43 +0545

    refactor: rename packages and classes for improved readability

--- src/main/java/np/com/krishnabk/mycoolapp/MycoolApplication.java
+++ src/main/java/np/com/krishnabk/mycoolapp/MycoolApplication.java
@@ -1,4 +1,4 @@
-package np.com.krishnabk.springboothibernate.mycoolapp;
+package np.com.krishnabk.mycoolapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/np/com/krishnabk/mycoolapp/controller/FunRestController.java
+++ src/main/java/np/com/krishnabk/mycoolapp/controller/FunRestController.java
@@ -1,4 +1,4 @@
-package np.com.krishnabk.springboothibernate.mycoolapp.rest;
+package np.com.krishnabk.mycoolapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

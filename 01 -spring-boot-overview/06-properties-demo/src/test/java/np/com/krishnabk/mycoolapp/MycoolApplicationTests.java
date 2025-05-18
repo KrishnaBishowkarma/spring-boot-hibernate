68a8bc70bd766822672afee4bc7dde84f28f3200
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-18 10:07:49 +0545

    Cleaned up .gitignore and removed ignored files

---
+++ 01 -spring-boot-overview/06-properties-demo/src/test/java/np/com/krishnabk/mycoolapp/MycoolApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.krishnabk.mycoolapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

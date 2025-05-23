b3a93e92604f8c3260b9cff8d2605467aeb83847
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-23 09:31:42 +0545

    feat(core): add component-scanning module with Coach, CricketCoach, and DemoController

---
+++ 02-spring-boot-spring-core/02-component-scanning/src/main/java/np/com/krishnabk/springcoredemo/rest/DemoController.java
@@ -0,0 +1,26 @@
+package np.com.krishnabk.springcoredemo.rest;
+
+import np.com.krishnabk.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for dependency injection
+    @Autowired
+    public  DemoController(Coach cricketCoach){
+        myCoach = cricketCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+
+}

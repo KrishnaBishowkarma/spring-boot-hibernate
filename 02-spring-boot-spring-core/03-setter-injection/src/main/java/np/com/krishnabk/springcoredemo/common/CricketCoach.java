5428c0e3e6de01220c6ed606ff9c4243c36ed366
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-24 13:56:25 +0545

    chore: add @Component annotation to CricketCoach for Spring bean registration

--- 02-spring-boot-spring-core/03-setter-injection/src/main/java/np/com/krishnabk/springcoredemo/common/CricketCoach.java
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/np/com/krishnabk/springcoredemo/common/CricketCoach.java
@@ -1,5 +1,8 @@
 package np.com.krishnabk.springcoredemo.common;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class CricketCoach implements Coach{
     @Override
     public String getDailyWorkout() {

6e137edb09dbe68d1291c8a3b4ad026baf4f8aeb
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-24 13:58:05 +0545

    feat: add DemoController with setter injection for Coach dependency

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/np/com/krishnabk/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package np.com.krishnabk.springcoredemo.rest;
+
+import np.com.krishnabk.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for dependency
+    private Coach myCoach;
+
+    // define a setter for dependency injection
+    @Autowired
+    public void setCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    // expose endpoint
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}

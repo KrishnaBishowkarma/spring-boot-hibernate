6cf7087b602d41b775e9df3f785363568590e78a
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-30 05:37:22 +0545

    feat: inject SwimCoach bean using Java-based configuration

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/np/com/krishnabk/springcoredemo/config/SportConfig.java
@@ -0,0 +1,15 @@
+package np.com.krishnabk.springcoredemo.config;
+
+import np.com.krishnabk.springcoredemo.common.Coach;
+import np.com.krishnabk.springcoredemo.common.SwimCoach;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SportConfig {
+
+    @Bean
+    public Coach swimCoach(){
+        return new SwimCoach();
+    }
+}
--- 02-spring-boot-spring-core/09-java-config-bean/src/main/java/np/com/krishnabk/springcoredemo/rest/DemoController.java
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/np/com/krishnabk/springcoredemo/rest/DemoController.java
@@ -14,7 +14,7 @@ public class DemoController {
     // define a constructor for dependency injection
     @Autowired
     public DemoController(
-            @Qualifier("trackCoach") Coach theCoach){
+            @Qualifier("swimCoach") Coach theCoach){
         System.out.println("In Constructor: " + getClass().getSimpleName());
         myCoach = theCoach;
     }

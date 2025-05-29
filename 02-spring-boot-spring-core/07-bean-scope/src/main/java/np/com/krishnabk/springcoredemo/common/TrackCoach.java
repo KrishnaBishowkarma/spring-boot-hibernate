70fe1a98dada1829736cb5e00dda6db2e0e74b93
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-05-29 10:18:07 +0545

    feat(scope): set TrackCoach bean scope to prototype
    - Applied @Scope annotation with ConfigurableBeanFactory.SCOPE_PROTOTYPE
    - Ensures a new instance of TrackCoach is created on each request

--- 02-spring-boot-spring-core/07-bean-scope/src/main/java/np/com/krishnabk/springcoredemo/common/TrackCoach.java
+++ 02-spring-boot-spring-core/07-bean-scope/src/main/java/np/com/krishnabk/springcoredemo/common/TrackCoach.java
@@ -1,8 +1,12 @@
 package np.com.krishnabk.springcoredemo.common;
 
+import org.springframework.aop.aspectj.annotation.PrototypeAspectInstanceFactory;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class TrackCoach implements Coach{
 
     public TrackCoach(){

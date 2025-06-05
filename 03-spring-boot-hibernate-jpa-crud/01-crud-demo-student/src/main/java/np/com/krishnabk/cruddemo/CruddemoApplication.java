3ad71ee696807739d5b6fc58dda2d8cda642f008
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-06-06 00:58:13 +0545

    feat: initialize Spring Boot CRUD demo project with basic structure

---
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/np/com/krishnabk/cruddemo/CruddemoApplication.java
@@ -0,0 +1,13 @@
+package np.com.krishnabk.cruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CruddemoApplication.class, args);
+    }
+
+}
---
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/test/java/np/com/krishnabk/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.krishnabk.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

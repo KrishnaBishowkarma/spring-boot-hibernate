9075d5d285ef11e92f54924029a8e59706ac588a
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-06-08 22:01:01 +0545

    feat(entity): add Student class with JPA annotations and fields

---
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/np/com/krishnabk/cruddemo/intity/Student.java
@@ -0,0 +1,30 @@
+package np.com.krishnabk.cruddemo.intity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+    // define fields
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    // define constructors
+
+    // define getters and setters
+
+    // define toString() method
+
+}

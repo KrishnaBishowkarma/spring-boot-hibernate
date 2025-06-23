e3db6e694a6bb01bad3815fcf4abf1502ffcb326
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-06-23 13:17:41 +0545

    feat: implement indAll method to DAO implementation

--- 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/np/com/krishnabk/cruddemo/dao/StudentDAOImpl.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/np/com/krishnabk/cruddemo/dao/StudentDAOImpl.java
@@ -1,10 +1,13 @@
 package np.com.krishnabk.cruddemo.dao;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import np.com.krishnabk.cruddemo.intity.Student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public class StudentDAOImpl implements StudentDAO{
@@ -28,5 +31,15 @@ public class StudentDAOImpl implements StudentDAO{
     @Override
     public Student findById(Integer id) {
         return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery("FROM Student", Student.class);
+
+        // return query results
+        return theQuery.getResultList();
     }
 }

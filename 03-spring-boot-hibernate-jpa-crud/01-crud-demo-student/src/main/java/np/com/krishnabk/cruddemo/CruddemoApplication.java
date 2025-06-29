45f534e6852badeca8925d66e61b19bb7da4dd4d
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-06-29 08:45:56 +0545

    feat: update the main application to delete all students from database

--- 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/np/com/krishnabk/cruddemo/CruddemoApplication.java
+++ 03-spring-boot-hibernate-jpa-crud/01-crud-demo-student/src/main/java/np/com/krishnabk/cruddemo/CruddemoApplication.java
@@ -31,8 +31,19 @@ public class CruddemoApplication {
 
 //              updateStudent(studentDAO);
 
-            deleteStudent(studentDAO);
+//            deleteStudent(studentDAO);
+
+            deleteAllStudents(studentDAO);
         };
+    }
+
+    private void deleteAllStudents(StudentDAO studentDAO) {
+
+        System.out.println("Deleting all students");
+        int numRowsDeleted = studentDAO.deleteAll();
+
+        System.out.println("Successfully deleted all the row (" + numRowsDeleted + ").");
+
     }
 
     private void deleteStudent(StudentDAO studentDAO) {

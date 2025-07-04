397d7f780b347dc1d30beb6903b675781040a749
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-07-04 09:28:36 +0545

    feat: add GET /students/{studentId} endpoint to return a student by index

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/np/com/krishnabk/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/np/com/krishnabk/demo/rest/StudentRestController.java
@@ -3,6 +3,7 @@ package np.com.krishnabk.demo.rest;
 import jakarta.annotation.PostConstruct;
 import np.com.krishnabk.demo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,4 +33,15 @@ public class StudentRestController {
 
         return theStudents;
     }
+
+    // define endpoint or "/students/{studentId}" - return student at index
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        // just index into the list ... keep it simple for now
+
+        return theStudents.get(studentId);
+    }
+
 }

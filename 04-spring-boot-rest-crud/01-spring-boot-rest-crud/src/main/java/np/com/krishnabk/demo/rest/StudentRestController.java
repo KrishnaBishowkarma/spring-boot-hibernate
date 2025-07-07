4efacd8f671a55f289e255281777e80d98dc66b5
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-07-07 07:52:05 +0545

    feat: add generic exception handler to return custom error response for all exceptions

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/np/com/krishnabk/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/np/com/krishnabk/demo/rest/StudentRestController.java
@@ -65,4 +65,20 @@ public class StudentRestController {
         // return ResponseEntity
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
+
+    // Add another exception handler ... to catch any exception (catch all)
+
+    @ExceptionHandler
+    public ResponseEntity<StudentErrorResponse> handleException(Exception e){
+
+        // create a StudentErrorResponse
+
+        StudentErrorResponse errorResponse = new StudentErrorResponse();
+        errorResponse.setStatus(HttpStatus.BAD_REQUEST.value());
+        errorResponse.setMessage(e.getMessage());
+        errorResponse.setTimeStamp(System.currentTimeMillis());
+
+        // return ResponseEntity
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
 }

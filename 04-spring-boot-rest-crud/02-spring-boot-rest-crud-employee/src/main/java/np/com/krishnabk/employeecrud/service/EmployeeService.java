f9cd9df296e0a53006198d7c201da97879df8c8d
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-07-11 14:34:23 +0545

    feat: add findById, save, and deleteById methods in EmployeeService

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/np/com/krishnabk/employeecrud/service/EmployeeService.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/np/com/krishnabk/employeecrud/service/EmployeeService.java
@@ -5,5 +5,12 @@ import np.com.krishnabk.employeecrud.entity.Employee;
 import java.util.List;
 
 public interface EmployeeService {
+
     List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }

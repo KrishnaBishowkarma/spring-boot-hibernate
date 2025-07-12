f0c05ea2a2a7ebe2b55400f42a57bdcd8f2183d6
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-07-12 07:14:37 +0545

    feat: add POST `/employees` endpoint to add new employee
    - Added `@PostMapping` to handle POST requests
    - Set employee ID to 0 to force a save of a new item instead of an update,
      in case an ID is accidentally passed in the JSON payload

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/np/com/krishnabk/employeecrud/rest/EmployeeRestController.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/np/com/krishnabk/employeecrud/rest/EmployeeRestController.java
@@ -3,10 +3,7 @@ package np.com.krishnabk.employeecrud.rest;
 import np.com.krishnabk.employeecrud.entity.Employee;
 import np.com.krishnabk.employeecrud.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,7 +24,7 @@ public class EmployeeRestController {
         return employeeService.findAll();
     }
 
-    // add maping for GET /employee/{employeeId}
+    // add mapping for GET /employees/{employeeId}
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable int employeeId){
 
@@ -36,6 +33,20 @@ public class EmployeeRestController {
         if (theEmployee == null){
             throw new RuntimeException("Employee id not found - " + theEmployee);
         }
+
+        return theEmployee;
+    }
+
+    // add mapping for POST /employees - add new employee
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee){
+
+        // also just in case they pass an id in JSON ... set id to 0
+        // this is to force a save of new item ... instead of update
+
+        theEmployee.setId(0);
+
+        Employee dbEmployee = employeeService.save(theEmployee);
 
         return theEmployee;
     }

b2615b105009df4e722ec0d0a3a199f1ade00495
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-07-22 13:30:18 +0545

    refactor: rename base package to com.np.krishnabk

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/np/krishnabk/springboot/cruddemo/service/EmployeeService.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/np/krishnabk/springboot/cruddemo/service/EmployeeService.java
@@ -1,6 +1,6 @@
-package com.luv2code.springboot.cruddemo.service;
+package com.np.krishnabk.springboot.cruddemo.service;
 
-import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.np.krishnabk.springboot.cruddemo.entity.Employee;
 
 import java.util.List;
 

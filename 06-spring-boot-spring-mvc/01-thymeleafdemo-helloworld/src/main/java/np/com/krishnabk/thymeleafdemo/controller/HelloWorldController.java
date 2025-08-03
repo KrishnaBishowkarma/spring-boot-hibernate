e2a1c5f0ba7c25410c2193b708109096f423534f
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-08-03 06:59:59 +0545

    feat(mvc): add form processing with @RequestParam in version 3

--- 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/np/com/krishnabk/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/np/com/krishnabk/thymeleafdemo/controller/HelloWorldController.java
@@ -4,6 +4,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
@@ -37,6 +38,25 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName,
+                                          Model model){
+
+        // convert data to all caps, handle null value
+        if (theName == null) {
+            theName = "";
+        }
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hello my friend from v3! " + theName;
 
         // add message to the model
         model.addAttribute("message", result);

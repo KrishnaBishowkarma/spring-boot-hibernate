b582ba1b8664a0985a2b5aad83ba1ae9258745df
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-08-19 10:34:40 +0545

    feat: add validation rule to `Customer` class

--- 06-spring-boot-spring-mvc/02-springmvc-validationdemo/src/main/java/np/com/krishnabk/springdemo/mvc/Customer.java
+++ 06-spring-boot-spring-mvc/02-springmvc-validationdemo/src/main/java/np/com/krishnabk/springdemo/mvc/Customer.java
@@ -1,9 +1,15 @@
 package np.com.krishnabk.springdemo.mvc;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public class Customer {
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private int freePasses;
 
     private String firstName;
 
@@ -25,5 +31,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }

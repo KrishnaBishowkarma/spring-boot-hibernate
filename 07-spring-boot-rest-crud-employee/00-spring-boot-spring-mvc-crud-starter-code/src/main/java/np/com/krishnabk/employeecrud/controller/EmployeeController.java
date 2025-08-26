9d0eadfbff4d332976f7ffbd8c2cc27aaab776c9
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-08-26 09:28:23 +0545

    feat(controller): add EmployeeController for listing employees in MVC

---
+++ 07-spring-boot-rest-crud-employee/00-spring-boot-spring-mvc-crud-starter-code/src/main/java/np/com/krishnabk/employeecrud/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+package np.com.krishnabk.employeecrud.controller;
+
+import np.com.krishnabk.employeecrud.entity.Employee;
+import np.com.krishnabk.employeecrud.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService theEmployeeService){
+        employeeService = theEmployeeService;
+    }
+
+    // add mapping for "/list"
+    @GetMapping
+    public String listEmployees(Model theModel){
+
+        // get the employee from db
+        List<Employee> theEmployee = employeeService.findAll();
+
+        // add to the spring model
+        theModel.addAttribute("employees", theEmployee);
+
+        return "list-employees";
+    }
+
+}

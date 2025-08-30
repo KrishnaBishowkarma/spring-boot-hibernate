87b1b5f6587240cc57787230090340fbf769cd58
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-08-30 10:42:56 +0545

    feat(security): add in-memory user details manager with sample users
    - Configured DemoSecurityConfig to provide three users (John, Mary, Riya) with various roles using InMemoryUserDetailsManager

--- 08-sprinb-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/np/com/krishnabk/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-sprinb-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/np/com/krishnabk/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -1,7 +1,36 @@
 package np.com.krishnabk.springboot.demosecurity.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class DemoSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+        UserDetails john = User.builder()
+                .username("John")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("Mary")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER")
+                .build();
+
+        UserDetails riya = User.builder()
+                .username("Riya")
+                .password("{noop}test123")
+                .roles("EMPLOYEE","MANAGER","ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, riya);
+    }
+
 }

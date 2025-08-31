bbc21c592959a4dc79951c97666597f8888af543
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-08-31 20:43:00 +0545

    feat(security): add SecurityFilterChain bean and custom login page config
    - Require authentication for all requests
    - Configure custom login page (/showMyLoginPage) and login processing URL (/authenticateTheUser)
    - Use new SecurityFilterChain bean style as per Spring Security 5.7+ recommendations

--- 08-sprinb-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/np/com/krishnabk/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-sprinb-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/np/com/krishnabk/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,12 @@ package np.com.krishnabk.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,6 +34,22 @@ public class DemoSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, riya);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/showMyLoginPage")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                        );
+        return http.build();
     }
 
 }

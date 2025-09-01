800bd83aa8da71202707773f47c86487e3c71a18
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-09-01 09:01:44 +0545

    feat(controller): add LoginController with custom login page mapping
    - Implemented LoginController with @GetMapping("/showMyLoginPage") to serve the custom login page (plain-login.html)

---
+++ 08-sprinb-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/np/com/krishnabk/springboot/demosecurity/controller/LoginController.java
@@ -0,0 +1,16 @@
+package np.com.krishnabk.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showMyLoginPage(){
+
+        return "plain-login";
+
+    }
+
+}

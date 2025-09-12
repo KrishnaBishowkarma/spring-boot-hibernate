6df259130ed57c88e41c47778d128de55f574342
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-09-12 10:28:26 +0545

    feat(entity): add InstructorDetail JPA entity with mappings and constructors
    - Created InstructorDetail class with JPA annotations for `instructor_detail` table
    - Added fields: id, youtubeChannel, hobby
    - Included constructors, getters, setters, and toString() method

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni/src/main/java/np/com/krishnabk/cruddemo/entity/InstructorDetail.java
@@ -0,0 +1,70 @@
+package np.com.krishnabk.cruddemo.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "instructor_detail")
+public class InstructorDetail {
+
+
+    // step 1. annotate the class as an entity and map to db table
+    // step 2. define table fields
+    // step 3. annotate the fields with db column names
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "youtube_channel")
+    private String youtubeChannel;
+
+    @Column(name = "hobby")
+    private String hobby;
+
+    // step 4. create constructors
+    public InstructorDetail(){
+
+    }
+
+    public InstructorDetail(String youtubeChannel, String hobby) {
+        this.youtubeChannel = youtubeChannel;
+        this.hobby = hobby;
+    }
+
+    // step 5. generate getters/setters methods
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getYoutubeChannel() {
+        return youtubeChannel;
+    }
+
+    public void setYoutubeChannel(String youtubeChannel) {
+        this.youtubeChannel = youtubeChannel;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    // step 6. generate toString() method
+
+    @Override
+    public String  toString() {
+        return "InstructorDetail{" +
+                "id=" + id +
+                ", youtubeChannel='" + youtubeChannel + '\'' +
+                ", hobby='" + hobby + '\'' +
+                '}';
+    }
+}

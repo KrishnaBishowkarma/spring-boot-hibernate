e5541162d89c4dadd9a72d376621668795079d02
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-09-27 06:51:44 +0545

    feat(course): add uni-directional one-to-many relationship with Review
    - Added a List<Review> field in Course entity with `@OneToMany` mapping.
    - Configured join column as course_id in Review table.
    - Implemented getter, setter, and addReview convenience method.
    - Enables associating multiple reviews to a course (uni-directional).

--- 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/np/com/krishnabk/cruddemo/entity/Course.java
+++ 09-spring-boot-jpa-advanced-mappings/04-jpa-one-to-many-uni/src/main/java/np/com/krishnabk/cruddemo/entity/Course.java
@@ -2,6 +2,9 @@ package np.com.krishnabk.cruddemo.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "course")
 public class Course {
@@ -19,6 +22,10 @@ public class Course {
                             CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviews;
 
     // define constructors
     public Course(){}
@@ -53,6 +60,24 @@ public class Course {
         this.instructor = instructor;
     }
 
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    // add convenience method
+
+    public void addReview(Review theReview){
+
+        if(reviews == null){
+            reviews = new ArrayList<>();
+        }
+
+        reviews.add(theReview);
+    }
 
     // define toString()
 

ed9a540e8580b952f5f21b6232306bb8c05fb041
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-10-09 22:39:54 +0545

    chore(project): copy 04-jpa-one-to-many-uni as base for 05-jpa-many-to-many
    - Added initial files and structure for 05-jpa-many-to-many (copied from 04-jpa-one-to-many-uni).
    - Updated project/module names to reflect the new section.
    - Serves as boilerplate to implement many-to-many mapping next.

---
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-many-to-many/src/main/java/np/com/krishnabk/cruddemo/dao/AppDAO.java
@@ -0,0 +1,36 @@
+package np.com.krishnabk.cruddemo.dao;
+
+import np.com.krishnabk.cruddemo.entity.Course;
+import np.com.krishnabk.cruddemo.entity.Instructor;
+import np.com.krishnabk.cruddemo.entity.InstructorDetail;
+
+import java.util.List;
+
+public interface AppDAO {
+
+    void save(Instructor theInstructor);
+
+    Instructor findInstructorById(int theId);
+
+    void deleteInstructorById(int theId);
+
+    InstructorDetail findInstructorDetailById(int theId);
+
+    void deleteInstructorDetailById(int theId);
+
+    List<Course> findCoursesByInstructor(int theId);
+
+    Instructor findInstructorByIdJoinFetch(int theId);
+
+    void update(Instructor tempInstructor);
+
+    void update(Course tempCourse);
+
+    Course findCourseById(int theId);
+
+    void deleteCourseById(int theId);
+
+    void save(Course theCourse);
+
+    Course findCourseAndReviewByCourseId(int theId);
+}
